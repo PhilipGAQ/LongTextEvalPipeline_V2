bea57c69cbfe25674f3f1f64331b81c75a069c8a
PhilipGAQ <113370525+PhilipGAQ@users.noreply.github.com>
2024-07-03 15:58:42 +0800

    create repository

---
+++ README.md
@@ -0,0 +1,5 @@
+# A pipeline to eval the whole pipeline of retrieval and reranker of bge-embedding and bge-reranker.
+
+## TODO:
+1. Get dataset.
+2. 
---
+++ test.py
@@ -0,0 +1,14 @@
+from pprint import pprint
+
+data = {
+    'name': 'John Doe',
+    'age': 30,
+    'jobs': ['developer', 'manager'],
+    'address': {
+        'street': '123 Main St',
+        'city': 'Anytown',
+        'state': 'Anystate'
+    }
+}
+
+print(data)
